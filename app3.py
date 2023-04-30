1c8160c96de8c2c8c27f21bf7daa0ae2e0dd764b
Santacecilia <jorgesantacecilia@gmail.com>
2023-04-30 15:55:47 -0300

    9

--- app3.py
+++ app3.py
@@ -510,22 +510,17 @@ def app5():
         # Tabla dataframe entero
         st.dataframe(dfp.style.format({"Superficie (has)":"{:.0f}", "Rinde":"{:,}", "Ingreso":"${:,}", "Costos directos":"${:,}", "Gastos comercialización":"${:,}", "Margen bruto":"${:,}"}))
 
-        data = (
-          {"label": "Revenue", "sublabel": "US$, in thousands",
-           "range": [150, 225, 300], "performance": [220,270], "point": [250]},
-          {"label": "Profit", "sublabel": "%", "range": [20, 25, 30],
-           "performance": [21, 23], "point": [26]},
-          {"label": "Order Size", "sublabel":"US$, average","range": [350, 500, 600],
-           "performance": [100,320],"point": [550]},
-          {"label": "New Customers", "sublabel": "count", "range": [1400, 2000, 2500],
-           "performance": [1000, 1650],"point": [2100]},
-          {"label": "Satisfaction", "sublabel": "out of 5","range": [3.5, 4.25, 5],
-           "performance": [3.2, 4.7], "point": [4.4]}
-        )
+        data = pd.read_json('https://cdn.rawgit.com/plotly/datasets/master/BulletData.json')
+        
+        measure_colors=['rgb(63,102,153)', 'rgb(120,194,195)']
+        range_colors=['rgb(245,225,218)', 'rgb(241,241,241)']
+        
         
         fig = ff.create_bullet(
-            data, titles='label', subtitles='sublabel', markers='point',
-            measures='performance', ranges='range', orientation='v',
+            data, orientation='h', markers='markers', measures='measures',
+            ranges='ranges', subtitles='subtitle', titles='title',
+            range_colors=range_colors,
+            measure_colors=measure_colors
         )
         right.plotly_chart(fig, use_container_width=True)
 
