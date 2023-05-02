e95fc34d9c8c63c5f5f095acea963e51e9e95f5d
Santacecilia <jorgesantacecilia@gmail.com>
2023-05-01 23:39:04 -0300

    9

--- app3.py
+++ app3.py
@@ -510,77 +510,77 @@ def app5():
         # Tabla dataframe entero
         st.dataframe(dfp.style.format({"Superficie (has)":"{:.0f}", "Rinde":"{:,}", "Ingreso":"${:,}", "Costos directos":"${:,}", "Gastos comercialización":"${:,}", "Margen bruto":"${:,}"}))
 
-    def bulletgraph(data=None, limits=None, labels=None, axis_label=None, title="Rindes por cultivo",
-                    size=(5, 3), palette=None, formatter=None, target_color="gray",
-                    bar_color="red", label_color="gray", show_title=True):
+        def bulletgraph(data=None, limits=None, labels=None, axis_label=None, title="Rindes por cultivo",
+                        size=(5, 3), palette=None, formatter=None, target_color="gray",
+                        bar_color="red", label_color="gray", show_title=True):
+            
+            # Determine the max value for adjusting the bar height
+            # Dividing by 10 seems to work pretty well
+            h = limits[-1] / 10
         
-        # Determine the max value for adjusting the bar height
-        # Dividing by 10 seems to work pretty well
-        h = limits[-1] / 10
-    
-        # Use the green palette as a sensible default
-        if palette is None:
-            palette = sns.light_palette("blue", len(limits), reverse=False)
-    
-        # Must be able to handle one or many data sets via multiple subplots
-        if len(data) == 1:
-            fig, ax = plt.subplots(figsize=size, sharex=True)
-        else:
-            fig, axarr = plt.subplots(len(data), figsize=size, sharex=True)
-    
-        # Add each bullet graph bar to a subplot
-        for idx, item in enumerate(data):
-    
-            # Get the axis from the array of axes returned when the plot is created
-            if len(data) > 1:
-                ax = axarr[idx]
-    
-            # Formatting to get rid of extra marking clutter
-            ax.set_aspect('equal')
-            ax.set_yticklabels([item[0]])
-            ax.set_yticks([1])
-            ax.spines['bottom'].set_visible(False)
-            ax.spines['top'].set_visible(False)
-            ax.spines['right'].set_visible(False)
-            ax.spines['left'].set_visible(False)
-    
-            prev_limit = 0
-            for idx2, lim in enumerate(limits):
-                # Draw the bar
-                ax.barh([1], lim - prev_limit, left=prev_limit, height=h,
-                        color=palette[idx2])
-                prev_limit = lim
-            rects = ax.patches
-            # The last item in the list is the value we're measuring
-            # Draw the value we're measuring
-            ax.barh([1], item[1], height=(h / 6), color=bar_color)
-    
-            # Need the ymin and max in order to make sure the target marker
-            # fits
-            ymin, ymax = ax.get_ylim()
-            ax.vlines(
-                item[2], ymin * .9, ymax * .9, linewidth=1.5, color=target_color)
-    
-        # Now make some labels
-        if labels is not None:
-            for rect, label in zip(rects, labels):
-                height = rect.get_height()
-                ax.text(
-                    rect.get_x() + rect.get_width() / 2,
-                    -height * .4,
-                    label,
-                    ha='center',
-                    va='bottom',
-                    color=label_color,
-                    fontsize=15)
-        if formatter:
-            ax.xaxis.set_major_formatter(formatter)
-        if axis_label:
-            ax.set_xlabel(axis_label)
-        if show_title:
-            if title:
-                fig.suptitle(title, fontsize=20)
-                fig.subplots_adjust(hspace=0)
+            # Use the green palette as a sensible default
+            if palette is None:
+                palette = sns.light_palette("blue", len(limits), reverse=False)
+        
+            # Must be able to handle one or many data sets via multiple subplots
+            if len(data) == 1:
+                fig, ax = plt.subplots(figsize=size, sharex=True)
+            else:
+                fig, axarr = plt.subplots(len(data), figsize=size, sharex=True)
+        
+            # Add each bullet graph bar to a subplot
+            for idx, item in enumerate(data):
+        
+                # Get the axis from the array of axes returned when the plot is created
+                if len(data) > 1:
+                    ax = axarr[idx]
+        
+                # Formatting to get rid of extra marking clutter
+                ax.set_aspect('equal')
+                ax.set_yticklabels([item[0]])
+                ax.set_yticks([1])
+                ax.spines['bottom'].set_visible(False)
+                ax.spines['top'].set_visible(False)
+                ax.spines['right'].set_visible(False)
+                ax.spines['left'].set_visible(False)
+        
+                prev_limit = 0
+                for idx2, lim in enumerate(limits):
+                    # Draw the bar
+                    ax.barh([1], lim - prev_limit, left=prev_limit, height=h,
+                            color=palette[idx2])
+                    prev_limit = lim
+                rects = ax.patches
+                # The last item in the list is the value we're measuring
+                # Draw the value we're measuring
+                ax.barh([1], item[1], height=(h / 6), color=bar_color)
+        
+                # Need the ymin and max in order to make sure the target marker
+                # fits
+                ymin, ymax = ax.get_ylim()
+                ax.vlines(
+                    item[2], ymin * .9, ymax * .9, linewidth=1.5, color=target_color)
+        
+            # Now make some labels
+            if labels is not None:
+                for rect, label in zip(rects, labels):
+                    height = rect.get_height()
+                    ax.text(
+                        rect.get_x() + rect.get_width() / 2,
+                        -height * .4,
+                        label,
+                        ha='center',
+                        va='bottom',
+                        color=label_color,
+                        fontsize=15)
+            if formatter:
+                ax.xaxis.set_major_formatter(formatter)
+            if axis_label:
+                ax.set_xlabel(axis_label)
+            if show_title:
+                if title:
+                    fig.suptitle(title, fontsize=20)
+                    fig.subplots_adjust(hspace=0)
 
         
         # Definir los límites para cada cultivo
