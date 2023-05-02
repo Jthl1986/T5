cdb7e91b5ba608e29230b36af61c58ff37494e4d
Santacecilia <jorgesantacecilia@gmail.com>
2023-05-01 23:19:34 -0300

    9

--- app3.py
+++ app3.py
@@ -593,13 +593,14 @@ def app5():
         data_to_plot = [(cultivo, rinde, cultivo_limits[cultivo], 4) for cultivo, rinde in zip(dfp["Cultivo"], dfp["Rinde"])]
         
         # Aumentar el tamaño de la fuente de los nombres de los cultivos
-        plt.rc('xtick', labelsize=25)
+        plt.rc('xtick', labelsize=10)
         
         # Crear el bullet chart para cada cultivo
         plt.suptitle("Rindes por cultivo", fontsize=14, fontname="sans-serif")
         colors = ['#fc0505', '#f7f7f7', '#2ca02c', '#ff7f0e']
         for cultivo in cultivo_limits.keys():
             cultivo_data = [(c, r, l, o) for c, r, l, o in data_to_plot if c == cultivo]
+            plt.ylabel("Cultivo", fontsize=14)
             if cultivo_data:
                 bulletgraph(cultivo_data, limits=cultivo_limits[cultivo], labels=["Bajo", "Medio", "Alto", "Objetivo"], size=(8,5),
                             label_color="black", bar_color=colors[0], target_color=colors[1])
