a42a2647f61b7afdff88d735adce06e688e879bd
Santacecilia <jorgesantacecilia@gmail.com>
2023-05-03 22:03:30 -0300

    9

--- app3.py
+++ app3.py
@@ -598,6 +598,7 @@ def app5():
         
         # Aumentar el tamaño de la fuente de los nombres de los cultivos
         plt.rc('xtick', labelsize=20)
+        plt.rc('ytick', labelsize=15)
         
         
         if data_to_plot is not None:
