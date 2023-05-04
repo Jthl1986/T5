18dd8e108c49ee051887b10a32f4d55da55ad3cd
Santacecilia <jorgesantacecilia@gmail.com>
2023-05-03 21:38:59 -0300

    9

--- app3.py
+++ app3.py
@@ -601,7 +601,7 @@ def app5():
         
         if data_to_plot is not None:
             texto = "Rendimiento por cultivo"
-            left.write(f"<span style='font-size: 12px; color: #808080; font-family: sans-serif;'>{texto}</span>", unsafe_allow_html=True)
+            left.write(f"<span style='font-size: 12px; color: #808080; font-family: Source Sans Pro, sans-serif;'>{texto}</span>", unsafe_allow_html=True)
         
         colors = ['#fc0505', '#f7f7f7', '#2ca02c', '#ff7f0e']
         for cultivo in cultivo_limits.keys():
