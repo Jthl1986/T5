9ae68dc4f9d52dd21e51406b277cf42cdc5bf7f2
Santacecilia <jorgesantacecilia@gmail.com>
2023-05-05 15:54:56 -0300

    9

--- app3.py
+++ app3.py
@@ -522,7 +522,8 @@ def app5():
         
             # Use the green palette as a sensible default
             if palette is None:
-                palette = sns.color_palette("hls", len(limits), h=.01, l=.6, s=.8)
+                palette = sns.color_palette("RdYlGn", len(limits))
+                palette = sns.light_palette(palette[0], n_colors=len(limits), reverse=False)
         
             # Must be able to handle one or many data sets via multiple subplots
             if len(data) == 1:
