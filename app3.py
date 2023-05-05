f2bfa153dc834cad8521bdc16952f8ce158e78cb
Santacecilia <jorgesantacecilia@gmail.com>
2023-05-05 15:59:02 -0300

    9

--- app3.py
+++ app3.py
@@ -514,7 +514,7 @@ def app5():
         
         def bulletgraph(data=None, limits=None, labels=None, axis_label=None, title="Rindes por cultivo",
                         size=(5, 3), palette=None, formatter=None, target_color="gray",
-                        bar_color="red", label_color="gray", show_title=True):
+                        bar_color="black", label_color="gray", show_title=True):
             
             # Determine the max value for adjusting the bar height
             # Dividing by 10 seems to work pretty well
