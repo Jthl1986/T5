c7cd34ac6045205e90362526fda29f023355c825
Santacecilia <jorgesantacecilia@gmail.com>
2023-05-05 16:12:54 -0300

    9

--- app3.py
+++ app3.py
@@ -518,7 +518,7 @@ def app5():
             
             # Determine the max value for adjusting the bar height
             # Dividing by 10 seems to work pretty well
-            h = 0
+            h = limits[-1] / 10
         
             # Use the green palette as a sensible default
             if palette is None:
