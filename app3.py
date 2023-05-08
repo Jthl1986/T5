5b2f5b7a0fac266a8fb1b514b66ce0c279a76767
Santacecilia <jorgesantacecilia@gmail.com>
2023-05-07 21:49:49 -0300

    9

--- app3.py
+++ app3.py
@@ -495,6 +495,11 @@ def app5():
         df_grouped = dfp.groupby('Cultivo')['Superficie (has)'].sum().reset_index()
         colors = px.colors.qualitative.Plotly
         fig = px.bar(df_grouped, x='Cultivo', y='Superficie (has)', color='Cultivo', color_discrete_sequence=colors)
+        
+        # Agregar texto en cada barra
+        for i, row in df_grouped.iterrows():
+            fig.data[0].text[i] = f"{row['Superficie (has)']:.0f} has"
+        
         # Ajustar el margen inferior y superior del gráfico
         fig.update_layout(margin=dict(t=0, b=0))
         right.plotly_chart(fig, use_container_width=True)
