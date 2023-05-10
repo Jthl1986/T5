e6e256afb0ddcac1e572f8cb9169a93aae65bbbf
Santacecilia <jorgesantacecilia@gmail.com>
2023-05-10 10:03:22 -0300

    9

--- app3.py
+++ app3.py
@@ -663,8 +663,6 @@ def app5():
         # Creamos un DataFrame con los cultivos y los meses del año
 
 
-
-
     df = pd.DataFrame({
         'Etapa': ['Siembra', 'Crecimiento', 'Cosecha'],
         'Inicio': ['2023-01-01', '2023-04-01', '2023-09-01'],
@@ -676,15 +674,16 @@ def app5():
     
     df['Mes'] = pd.to_datetime(df['Inicio']).dt.strftime('%b')
     
-    fig = px.timeline(df, x_start='Inicio', x_end='Fin', base='Etapa', color='Etapa', 
+    fig = px.timeline(df, x_start='Inicio', x_end='Fin', y='Etapa', color='Etapa', 
                       color_discrete_sequence=['#008000', '#FFA500', '#FF0000'], 
                       hover_name='Etapa', hover_data={'Inicio': '|%B %d, %Y', 'Fin': '|%B %d, %Y'}, 
-                      range_x=['2023-01-01', '2023-12-31'], width=800, height=400)
+                      range_x=['2023-01-01', '2023-12-31'], width=800, height=400,
+                      facet_row='Etapa')
     
     fig.update_yaxes(autorange="reversed")
     fig.update_layout(title='Crop Calendar 2023', title_x=0.5, title_y=0.9, font=dict(size=14))
-    
     st.plotly_chart(fig)
+
 
 
     if dfp is not None and df1 is None:
